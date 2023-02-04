fef98c67318e527a134c19d8d3bbe034e8274228
Gabriele Pongelli <gpongelli@users.noreply.github.com>
2023-02-04 08:54:27 +0100

    Simplified entrypoint

--- entrypoint.sh
+++ entrypoint.sh
@@ -14,25 +14,16 @@ PGID=${PGID:-50}
 # enforce ownership if running as root
 user="$(id -u)"
 if [ "$user" = '0' ]; then
-    # https://github.com/sudo-bmitch/jenkins-docker/blob/main/entrypoint.sh
-    # get gid of docker socket file
-    SOCK_DOCKER_GID=`ls -ng /var/run/docker.sock | cut -f3 -d' '`
-
-    # get group of docker inside container
-    CUR_DOCKER_GID=`getent group docker | cut -f3 -d: || true`
-
-    # if they don't match, adjust
-    if [ ! -z "$SOCK_DOCKER_GID" -a "$SOCK_DOCKER_GID" != "$CUR_DOCKER_GID" ]; then
-        groupmod -g ${SOCK_DOCKER_GID} -o docker
-    fi
-    if ! groups pgadmin4 | grep -q docker; then
-        usermod -aG docker pgadmin4
+    # folder absent
+    if [ ! -d "$HOME"/config ]; then
+        mkdir -p $HOME/config
     fi
 
-    # $HOME folder absent
-    if [ ! -d "$HOME" ]; then
-        mkdir -p $HOME/config $HOME/storage
+    # folder absent
+    if [ ! -d "$HOME"/storage ]; then
+        mkdir -p $HOME/storage
     fi
+
     chown -Rc "$PUID:$PGID" "$HOME"
 
     # Add call to gosu to drop from root user to pgadmin4 user
